879dd83dcf6d14ff9c337f519cc3f81f2f17f04f
KseniaPaz <knopik-ksu@mail.ru>
2023-11-27 19:26:28 +0500

    HW1 added

---
+++ Homework1/Task2.py
@@ -0,0 +1,7 @@
+# Задача 2.
+# Написать точно такую же процедуру, но в декларативном стиле.
+
+def quick_sort(numbers):
+    return sorted(numbers, reverse=True)
+
+print("Отсортированный список:", quick_sort)
